c021397d5874d07afb232695b877dae18d2b40f9
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-07-24 22:35:20 +0000

    Funcionando?

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const rls = require('readline-sync');
 var nome = rls.question("qual seu nome?");
 
 //var nonr recebe o valor processado pela question que é um método da rls, que é um exemplo re monstro objeto da classe de java aaaaa
-conseole.log('seu nome é: '+nome+'!');
+console.log('seu nome é: ' + nome + '!');
 
+//quando estava "consEole", deu como obejto não encontrado. a importância do [[erro]].
+
+//conheço as prototypes, primitivas, mas n me lembro de destruí-la.
